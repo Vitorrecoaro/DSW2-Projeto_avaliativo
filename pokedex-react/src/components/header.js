5bf52f9392cabdf73f01044fb9cb57aa4f4c6302
MatheusRMVideira <matheus.rezendev@gmail.com>
2023-03-23 12:19:21 -0300

    Corrigindo responsividade na página de pesquisa

--- pokedex-react/src/components/header.js
+++ pokedex-react/src/components/header.js
@@ -1,5 +1,6 @@
-import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { MDBIcon } from "mdb-react-ui-kit";
 import "../stylesheet/default.css";
 import "../stylesheet/header.css";
 import searchIcon from "../images/search_icon.png";
@@ -8,6 +9,7 @@ import header_image from "../images/header.png";
 
 function Header(props) {
     const navigate = useNavigate();
+    const [searchOpen, setSearchOpen] = useState(false);
 
     function getSearch() {
         const search = document.getElementById("searchField").value;
@@ -29,25 +31,57 @@ function Header(props) {
         } else {
             navigate("/search/" + data);
         }
+        setSearchOpen(false);
     }
+
+    function handleMobileButtonClick() {
+        setSearchOpen(!searchOpen);
+    }
+
+    useEffect(() => {
+        if (searchOpen) {
+            document.body.style.overflow = "hidden";
+            document.addEventListener("keydown", (event) => {
+                if (event.key === "Escape") {
+                    setSearchOpen(false);
+                }
+            });
+        } else {
+            document.body.style.overflow = "unset";
+            document.removeEventListener("keydown", (event) => {
+                if (event.key === "Escape") {
+                    setSearchOpen(false);
+                }
+            }
+            );
+        }
+    }, [searchOpen]);
 
 
     return (
-        <div className="header grid-header">
+        <><div className="header grid-header">
             <Link to="/" className="grid-header-logo">
                 <img className="header-img" src={header_image} alt="Header" />
             </Link>
             <div className="grid-header-nav"></div>
-            <div className="grid-header-search-full">
-                <input type="text" id="searchField" placeholder="Procurar" onKeyDown={handleEnterPress} value={props.searchPlaceholder}/>
+            {!searchOpen && <div className="grid-header-search-full">
+                <input type="text" id="searchField" placeholder="Procurar" onKeyDown={handleEnterPress} value={props.searchPlaceholder} />
                 <img className="header-search-icon" src={searchIcon} alt="Search" onClick={getSearch} />
-            </div>
+            </div>}
             <div className="grid-header-mobile">
                 <div className="grid-header-search-mobile">
-                    <img className="header-search-icon" src="../images/search_icon.png" alt="Search" />
+                    <MDBIcon fas icon="search" size="lg" onClick={handleMobileButtonClick} />
                 </div>
             </div>
         </div>
+            {searchOpen && <>
+                <div className="dimmed-background" onClick={handleMobileButtonClick}></div>
+                <dialog className="dialog-search-mobile" open>
+                    <div className="dialog-search-mobile-input">
+                        <input type="text" id="searchField" placeholder="Procurar" onKeyDown={handleEnterPress} value={props.searchPlaceholder} autoFocus/>
+                        <img className="header-search-icon" src={searchIcon} alt="Search" onClick={getSearch} />
+                    </div>
+                </dialog></>}</>
     );
 }
 
